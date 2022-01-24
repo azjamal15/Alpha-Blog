310b00584bf03e66961df921a6ea487aa91ba04a
azjamal15 <azamj13@gmail.com>
2022-01-24 11:33:04 -0600

    Adds validations to Article columns

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,4 @@
 class Article < ApplicationRecord
-
+  validates :title, presence: true, length: { minimum: 6, maximum: 100 } #makes sure title is present for article to be saved 
+  validates :description, presence: true, length: { minimum: 10, maximum: 300 }
 end
